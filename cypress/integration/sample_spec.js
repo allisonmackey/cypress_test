746d0a545cb56363a614e54dd6f361bbf0561852
Allison Mackey <a.mackey693@gmail.com>
2020-08-18 16:40:22 -0700

    add second test to visit cypress example page, test passes

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,12 @@
-describe('My First Test', () => {
+describe('My First Test Block', () => {
 	it('Does not do much!', () => {
 		expect(true).to.equal(true)
 	})
+
+	it('Visits the Kitchen Sink', () => {
+		cy.visit('https://example.cypress.io')
+	})
+
+
+
 })
